4d9b72fb56eb81710ce81ad808925a60133c8113
unknown <mabroukkhadija566@gmail.com>
2024-08-13 15:37:50 +0100

    first commit

---
+++ GestionReservation/src/main/java/com/example/GestionReservation/GestionReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionReservationApplication.class, args);
+	}
+
+}
